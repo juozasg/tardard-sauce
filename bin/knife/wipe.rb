e4005682c5c81018ca0da54bdc39d9f28a1724fb
Juozas Gaigalas <juozasgaigalas@gmail.com>
2013-06-18 16:43:06 -0700

    knife plugins: wipe out chef node

---
+++ bin/knife/wipe.rb
@@ -0,0 +1,30 @@
+require 'chef/knife'
+
+class Chef
+  class Knife
+    class Wipe < Knife
+
+      deps do
+        require 'chef/api_client'
+        require 'chef/json_compat'
+      end
+
+      banner "knife wipe CLIENT (options)"
+
+      def run
+        @client_name = @name_args[0]
+        config[:yes] = true
+
+        if @client_name.nil?
+          show_usage
+          ui.fatal("You must specify a client name")
+          exit 1
+        end
+
+        delete_object(Chef::Node, @client_name)
+        delete_object(Chef::ApiClient, @client_name)
+      end
+
+    end
+  end
+end
